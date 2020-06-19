4efd4f01589a2760493dadab9a7d68cf97cbcb65
christiano39 <carneson39@gmail.com>
2020-06-19 09:34:54 -0600

    added routes and link to /pizza

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch } from 'react-router-dom'
+import Home from './Components/Home'
+import PizzaForm from './Components/PizzaForm'
+import * as Yup from 'yup'
+
+const initialFormData = {
+  name: '',
+  size: '',
+  toppings: {
+    peperoni: false,
+    olives: false,
+    mushrooms: false,
+    bacon: false,
+  },
+  instructions: '',
+}
 
 const App = () => {
+  const [formData, setFormData] = useState(initialFormData)
+
+  const onInputChange = e => {
+    const { name, value } = e.target
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const onCheckboxChange = e => {
+    const { name, checked } = e.target
+
+    setFormData({
+      ...formData,
+      toppings: {
+        ...formData.toppings,
+        [name]: checked,
+      }
+    })
+  }
+  
   return (
-    <>
+    <div className='app'>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      
+      <Switch>
+        <Route path='/pizza'>
+          <PizzaForm />
+        </Route>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+
+    </div>
   );
 };
 export default App;
---
+++ src/Components/PizzaForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function PizzaForm(props){
+    return (
+        <div className='order-form'>
+            <h2>Create your pizza</h2>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
